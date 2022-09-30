a98a9c03af6504dbfcb65155c34a9d2799e4a706
adwaithks <adwaith2507@gmail.com>
2022-09-30 18:28:53 +0530

    removing cache logic in serviceworker for fetch

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,9 +11,5 @@ self.addEventListener("activate", (e) => {
 });
 
 self.addEventListener("fetch", (e) => {
-    e.respondWith(
-        caches.match(e.request).then((res) => {
-            return res || fetch(e.request);
-        })
-    );
+
 });
