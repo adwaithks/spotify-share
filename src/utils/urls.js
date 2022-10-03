5bc9e1a7b96098b0da3591acb163dbcfd8f9ccfb
adwaithks <adwaith2507@gmail.com>
2022-10-03 17:57:33 +0530

    http to https

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,4 +1,4 @@
 export const URLS = {
-    REDIRECT_URL: 'http://spotify-share.netlify.app/',//'https://localhost:3000/',
+    REDIRECT_URL: 'https://spotify-share.netlify.app/',//'https://localhost:3000/',
     BASE_URL: 'https://spotify-share-server.vercel.app'//'https://spotify-share-server.vercel.app',
 }
